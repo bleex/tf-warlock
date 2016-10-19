7164012369785086cd90da9401d7db7eb50d747e
bleex <9681306+bleex@users.noreply.github.com>
2016-10-19 23:35:32 +0200

    śro, 19 paź 2016, 23:35:32 CEST

--- lib/gmcp.tf
+++ lib/gmcp.tf
@@ -8,5 +8,24 @@
 
 ;; Initial define to get some data to have foundation to work on scripts
 
-/def -hGMCP gmcp_receive = /echo GMCP: %*
+;;/def -hGMCP gmcp_receive = /echo GMCP: %{*} %;\
+/def -hGMCP gmcp_receive = \
+	/if (regmatch("Room.([a-zA-Z]+) { (.*) }$", {*}) > 0) \
+		/test $[process_room({P1},{P2})] %;\
+	/endif
 
+
+;; INFO: "wewnatrz": 0, "rodzaj": 8, "obszar": "Sithe, lokacje", "idprev": "$1$$a5SudpvkmEnsbgN4VGrpR0", "domena": "Orchia", "id": "$1$$\/u\/vtMpZQbCieNa4CYDWM1", "komenda": [ "S", "na wschod", "wschod", 1 ]
+
+/def process_room = \
+	/if ({1} =~ "Glance") \
+;;/echo GLANCE: %{2} %;\
+		/test %;\
+	/else \
+	/if ({1} =~ "Info") \
+	       /echo INFO: %{2} %;\
+;;/let i $[regmatch('"wewnatrz":([0-9]), "obszar": "([a-zA-Z0-9, ]+)", "idprev": "([^\"]+)", "domena": "([a-zA-Z]+)", "id": "([^\"]+)", "komenda": .*', {2})] %;\
+	       /let i $[regmatch('\"wewnatrz\"\:([0-9]), .*', {2})] %;\
+	       /vecho i %;\
+        /endif %;\
+	/endif	       
